4ff99cf98b87e4fdbd5e6a8354be1165e8b7f8b9
Rishikesh <107820362+Creepyrishi@users.noreply.github.com>
2023-10-19 21:44:13 +0545

    app completed

---
+++ esp/esp.py
@@ -0,0 +1,55 @@
+import urequests as requests
+import network
+import machine
+import ujson
+
+
+# Define the GPIO pin for the built-in LED
+led_pin = machine.Pin(2, machine.Pin.OUT)
+
+
+# Function to connect to Wi-Fi
+def connect_to_wifi(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    if not wlan.isconnected():
+        print("Connecting to Wi-Fi...")
+        wlan.active(True)
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            pass
+    print("Connected to Wi-Fi")
+
+# Firebase Realtime Database URL
+FIREBASE_URL = "https://chicken-farm-2af5d-default-rtdb.firebaseio.com/switches.json"
+
+# Function to fetch data from Firebase
+def fetch_data_from_firebase(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+# Configure your Wi-Fi credentials
+WIFI_SSID = "Moon"
+WIFI_PASSWORD = "NO SIGNAL"
+
+# Connect to Wi-Fi
+connect_to_wifi(WIFI_SSID, WIFI_PASSWORD)
+
+# Make a GET request to fetch data from Firebase
+while(True):
+    firebase_data = fetch_data_from_firebase(FIREBASE_URL)
+
+    if firebase_data is not None:
+        print("Data from Firebase:")
+        print(firebase_data)
+    else:
+        print("Failed to fetch data from Firebase.")
+    data =ujson.loads(firebase_data)
+    if data["eggs"] == "1":
+        led_pin.on()  # Turn on the LED
+    else:
+        led_pin.off()  # Turn off the LED
+  # Wait for 1 second
+
